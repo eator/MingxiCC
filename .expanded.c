076512827d13a3e8ca666695e0aedead6eeca937
mingxi-vmu <mingxi01forever@gmail.com>
2023-05-20 17:41:53 +0800

    change gitignore

--- .expanded.c
+++ .expanded.c
@@ -1,7 +1,11 @@
-# 0 "test_src/not_4__ret1.c"
+# 0 "test_src/function__ret2.c"
 # 0 "<built-in>"
 # 0 "<command-line>"
 # 1 "/usr/include/stdc-predef.h" 1 3 4
 # 0 "<command-line>" 2
-# 1 "test_src/not_4__ret1.c"
-int main() { return !!1; }
+# 1 "test_src/function__ret2.c"
+int one() { return 1; }
+
+int add() { return one() + 1; }
+
+int main() { return add(); }
